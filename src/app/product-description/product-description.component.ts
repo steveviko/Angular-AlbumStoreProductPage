6d937a38a5a576b1334f97c21378c6546af5d6e2
steveviko <stevenviko@gmail.com>
2019-11-05 18:19:33 +0300

    Factor product service file

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -7,7 +7,8 @@ import { ProductService} from '../product.service'
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-  albumInfo; 
+  albumInfo;
+
   constructor(private _productService:ProductService) {
 
    }
