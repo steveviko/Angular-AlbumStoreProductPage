3b4ece51e122f4f780a9d3e4fdef652654d249f1
steveviko <stevenviko@gmail.com>
2019-11-06 00:19:35 +0300

    Add a Route To The ProductPageComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { RouterModule,Routes } from "@angular/router";
 
 const appRoutes: Routes = [
   {path: 'products', component:ProductListComponent},
-  
+  {path: 'product/:id', component:ProductPageComponent}
 ]
 
 @NgModule({
