abff09a3322f0f04a4de0de3278871b5c17a9d97
steveviko <stevenviko@gmail.com>
2019-11-06 00:15:13 +0300

    Add an Array To Hold Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,10 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
 import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule,Routes } from "@angular/router";
 
+const appRoutes: Routes = [
+  
+]
+
 @NgModule({
   declarations: [
     AppComponent,
